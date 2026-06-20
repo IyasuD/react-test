82d7227831a3337a37324beba3327a978e9becd8
IyasuD <iyasu.geleta@gmail.com>
2026-06-19 18:38:38 -0600

    Add sub heading

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1>Learn react with liyutech.com</h1>
+      <h1>Learn react with liyutech.org</h1>
+      <p>Seek wisdom, Elevate your intellect and serve humanity.</p>
     </>
   );
 }
